20aa254e375b16eea659a922a3394db2489c8d07
Mekazstan <mekastans@gmail.com>
2022-12-24 09:23:20 -0800

    01 commit

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 import openai
 
+# Start with reading API KEY to enable conversation 
 def open_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read()
     
-open_api_key = open_file('api_key.txt')
+openaiapi_key = open_file('api_key.txt')
 
+# Process of sending PROMPT to the COMPLETIONS ENDPOINT
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
     prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
     try: 
@@ -26,6 +28,6 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
     
 
 if __name__ == '__main__':
-    prompt = 'Write an AI project idea: '
+    prompt = 'Write a list of business ideas using GPT-3: '
     response = gpt3_completion(prompt)
     print(response)
