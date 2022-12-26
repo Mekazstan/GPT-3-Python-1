c2b3fa21b4b4d3b4de56d11aaf841df26e899c20
Mekazstan <mekastans@gmail.com>
2022-12-26 10:59:20 -0800

    update comit

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ def open_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read()
     
-openaiapi_key = open_file('api_key.txt')
+openai.api_key = open_file('api_key.txt')
 
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['USER:', 'MEKABOT:']):
     prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
@@ -27,22 +27,26 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
 
 if __name__ == '__main__':
     conversation = list()
+    converse = True
     
     # Creating an open ended chat with while loop
-    while True:
+    while converse:
         # Get user's PROMPT
         user_input = input('USER: ')
-        # Add to the conversation list for display
-        conversation.append(f"USER: {user_input}")
-        # Format display 
-        text_block = '\n'.join(conversation)
-        # Setting prompt to info for the machine to work on
-        prompt = open_file('prompt_chat.txt').replace('<<BLOCK>>', text_block)
-        # Set bot conversation prompt
-        prompt = prompt + '\nMEKABOT:'
-        # Calling the completions endpoint and passing the prompt
-        response = gpt3_completion(prompt)
-        # Display on screen
-        print('MEKABOT:', response)
-        # Adding bots response to conversation list
-        conversation.append(f"MEKABOT: {response}")
+        if user_input.lower() == 'end':
+            converse = False
+        else:
+            # Add to the conversation list for display
+            conversation.append(f"USER: {user_input}")
+            # Format display 
+            text_block = '\n'.join(conversation)
+            # Setting prompt to info for the machine to work on
+            prompt = open_file('prompt_chat.txt').replace('<<BLOCK>>', text_block)
+            # Set bot conversation prompt
+            prompt = prompt + '\nMEKABOT:'
+            # Calling the completions endpoint and passing the prompt
+            response = gpt3_completion(prompt)
+            # Display on screen
+            print('MEKABOT:', response)
+            # Adding bots response to conversation list
+            conversation.append(f"MEKABOT: {response}")
--- index.py
+++ index.py
@@ -5,7 +5,7 @@ def open_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read()
     
-openaiapi_key = open_file('api_key.txt')
+openai.api_key = open_file('api_key.txt')
 
 # Process of sending PROMPT to the COMPLETIONS ENDPOINT
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
